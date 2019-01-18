5470a1c497af7f4b7449eb202febe13bc6c12036
Keith Wong <42085249+keithwongg@users.noreply.github.com>
2019-01-18 08:44:29 +0800

    Delete collect_executioner.py

--- BIA_STKJ/Steam_2k_Scrape/collect_executioner.py
+++
@@ -1,9 +0,0 @@
-from king_extractor import steam_comments_king_extractor
-import lxml.html
-import requests
-
-# Dark souls 3 id : 374320 # max 4137
-# No man's sky id : 275850 # max >5000
-
-steam_comments_king_extractor(374320, 200) # later remember to try this again 
-steam_comments_king_extractor(275850, 200)
